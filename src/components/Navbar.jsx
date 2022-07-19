e789e6d08d5c6bbe3351b68a48d813eca14dfb9b
JuanR34 <juanpablo_jpr@hotmail.com>
2022-07-19 17:37:21 -0300

    desafio2

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+export default function NavBar () {
+    return (
+        <div>
+            <ul>
+                <li>
+                <a class="" href=""> Home </a>
+                </li>
+                <li>
+                    <a class="" href=""> Nosotros </a>
+                </li>
+                <li>
+                <a class="" href=""> Contacto </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
